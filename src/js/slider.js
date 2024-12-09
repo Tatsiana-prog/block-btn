ded8b80d4b63af190d374b0d97dcebe0724474f2
ai-smartsite <aismartsite25@gmail.com>
2024-12-09 20:06:50 +0300

    corrected code

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,116 +1,147 @@
-// set and cache variables
-var w, container, carousel, item, radius, itemLength, rY, ticker, fps; 
-var mouseX = 0;
-var mouseY = 0;
-var mouseZ = 0;
-var addX = 0;
-var isMouseInsideCarousel = false; // флаг для отслеживания, находится ли курсор на карусели
+var radius = 415; // Размер радиуса 
+var autoRotate = false; // Отключаем автоматическое вращение по умолчанию
+var imgWidth = 314;
+var imgHeight = 298; // Высота изображений
+let autoSpinInterval;  // Интервал для автоматической прокрутки
+let currentRotationIndex = 0;  // Индекс текущего положения карусели
+var odrag = document.getElementById('drag-container');
+var ospin = document.getElementById('spin-container'); 
+var aImg = ospin.getElementsByTagName('img');
+var aVid = ospin.getElementsByTagName('video');
+var aEle = [...aImg, ...aVid]; // объединить 2 массива
 
-// fps counter
-var fps_counter = {
-  tick: function () 
-  {
-    this.times = this.times.concat(+new Date());
-    var seconds, times = this.times;
-    
-    if (times.length > this.span + 1) 
-    {
-      times.shift(); // ditch the oldest time
-      seconds = (times[times.length - 1] - times[0]) / 1000;
-      return Math.round(this.span / seconds);
-    } 
-    else return null;
-  },
+// Размер изображений
+ospin.style.width = imgWidth + "px";
+ospin.style.height = imgHeight + "px";
 
-  times: [],
-  span: 20
+// Размер земли - зависит от радиуса
+var ground = document.getElementById('ground');
+ground.style.width = radius * 3 + "px";
+ground.style.height = radius * 3 + "px";
+
+// Функция инициализации с настройкой начальной позиции второго изображения
+function init(delayTime) {
+  var startIndex = 1; // Начинаем с второго изображения (index 1)
+  for (var i = 0; i < aEle.length; i++) {
+    // Вычисляем угол для каждого элемента
+    var angle = (i + startIndex) * (360 / aEle.length);
+    aEle[i].style.transform = "rotateY(" + angle + "deg) translateZ(" + radius + "px)";
+    aEle[i].style.transition = "none";  // Убираем анимацию
+    // Применяем фильтр ко всем элементам, кроме центрального
+    //aEle[i].style.opacity = '1';
+  }
+}
+// Вызываем функцию инициализации после задержки
+setTimeout(init, 1000);
+
+// Обработчик перемещения мыши для вращения карусели
+var sX, sY, nX, nY, desX = 0, desY = 0, tX = 0, tY = 10;
+document.onpointerdown = function (e) {
+  clearInterval(odrag.timer);
+  e = e || window.event;
+  sX = e.clientX;
+  sY = e.clientY;
+  this.onpointermove = function (e) {
+    e = e || window.event;
+    nX = e.clientX;
+    nY = e.clientY;
+    desX = nX - sX;
+    desY = nY - sY;
+    tX += desX * 0.1;
+    tY += desY * 0.1;
+
+    // Ограничение вращения по оси X (вертикальная ось) на 60 градусов
+    if (tY > 30) tY = 30;
+    if (tY < 0) tY = 0;
+
+    applyTranform(odrag);
+    sX = nX;
+    sY = nY;
+  };
+
+  this.onpointerup = function () {
+    odrag.timer = setInterval(function () {
+      desX *= 0.95;
+      desY *= 0.95;
+      tX += desX * 0.1;
+      tY += desY * 0.1;
+
+      // Ограничение вращения по оси X (вертикальная ось) на 60 градусов
+      if (tY > 60) tY = 60;
+      if (tY < 0) tY = 0;
+
+      applyTranform(odrag);
+      playSpin(false);
+      if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
+        clearInterval(odrag.timer);
+        playSpin(true);
+      }
+    }, 17);
+    this.onpointermove = this.onpointerup = null;
+  };
+  return false;
 };
 
-var counter = Object.create(fps_counter);
+function applyTranform(obj) {
+  obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
+}
 
-$(document).ready(init);
+function playSpin(yes) {
+  ospin.style.animationPlayState = (yes ? 'running' : 'paused');
+}
 
-function init() {
-  w = $(window);
-  container = $('#contentContainer');
-  carousel = $('#carouselContainer');
-  item = $('.carouselItem');
-  itemLength = $('.carouselItem').length;
-  fps = $('#fps');
-  rY = 360 / itemLength;
-  radius = Math.round((190) / Math.tan(Math.PI / itemLength));
+document.onmousewheel = function (e) {
+  e = e || window.event;
+  var d = e.wheelDelta / 20 || -e.detail;
+  radius += d;
+  init(1);
+};
 
-  // set container 3d props
-  TweenMax.set(container, {perspective: 600});
-  TweenMax.set(carousel, {z: -(radius)});
+// Функция для переключения позиции карусели
+function rotateCarousel(index) {
+  // Рассчитываем угол для нужной карточки
+  let angle = index * (360 / aEle.length); 
+  aEle.forEach((ele, i) => {
+    // Применяем вращение для каждого элемента по углу
+    ele.style.transition = 'transform 1s';
+    ele.style.transform = `rotateY(${(i - index) * (360 / aEle.length)}deg) translateZ(${radius}px)`;
+    // Если это текущий элемент (центральная карточка), убираем фильтр
+    //if (i === index) {
+      //ele.style.opacity = '1';  // Убираем фильтр с текущего элемента
+    //} else {
+      //ele.style.opacity = '1';
+      //ele.style.filter = 'brightness(100%);'  // Применяем фильтр ко всем остальным
+    //}
+  });
+  currentRotationIndex = index; // Обновляем текущий индекс
+  // После клика запускаем автоматическую прокрутку
+  startAutoRotate();
+}
 
-  // create carousel item props
-  for (var i = 0; i < itemLength; i++) {
-    var $item = item.eq(i);
-    var $block = $item.find('.carouselItemInner');
-    
-    // set initial positions
-    TweenMax.set($item, {rotationY: rY * i, z: radius, transformOrigin: "50% 50% " + -radius + "px"});
-    animateIn($item, $block);
+// Вспомогательная функция для смены текущего индекса и переключения (по часовой стрелке)
+function changeCarouselIndex() {
+  var newIndex = currentRotationIndex + 1; // Сдвигаем вправо (по часовой стрелке)
+  if (newIndex >= aEle.length) {
+    newIndex = 0; // Переключаем на первый элемент, если индекс выходит за пределы
   }
-
-  // set mouse x and y props and looper ticker
-  window.addEventListener("mousemove", onMouseMove, false);
-  carousel.on('mouseenter', onMouseEnterCarousel); // мышь вошла на карусель
-  carousel.on('mouseleave', onMouseLeaveCarousel); // мышь покинула карусель
-  ticker = setInterval(looper, 1000 / 60); // обновление каждый 60-й кадр
+  rotateCarousel(newIndex); // Вращаем карусель на новый индекс
 }
 
-function animateIn($item, $block) {
-  var $nrX = 360 * getRandomInt(2);
-  var $nrY = 360 * getRandomInt(2);
-    
-  var $nx = -(2000) + getRandomInt(4000);
-  var $ny = -(2000) + getRandomInt(4000);
-  var $nz = -4000 + getRandomInt(4000);
-    
-  var $s = 1.5 + (getRandomInt(10) * .1);
-  var $d = 1 - (getRandomInt(8) * .1);
-  
-  TweenMax.set($item, { autoAlpha: 1, delay: $d });  
-  TweenMax.set($block, { z: $nz, rotationY: $nrY, rotationX: $nrX, x: $nx, y: $ny, autoAlpha: 0 });
-  TweenMax.to($block, $s, { delay: $d, rotationY: 0, rotationX: 0, z: 0, ease: Expo.easeInOut });
-  TweenMax.to($block, $s - .5, { delay: $d, x: 0, y: 0, autoAlpha: 1, ease: Expo.easeInOut });
-}
-
-function onMouseEnterCarousel() {
-  isMouseInsideCarousel = true; // мышь внутри карусели
-}
-
-function onMouseLeaveCarousel() {
-  isMouseInsideCarousel = false; // мышь покинула карусель
-}
-
-function onMouseMove(event) {
-  // если мышь внутри карусели, обновляем координаты
-  if (isMouseInsideCarousel) {
-    mouseX = -(-(window.innerWidth * .5) + event.pageX) * .0025;
-    mouseY = -(-(window.innerHeight * .5) + event.pageY) * .01;
-    mouseZ = -(radius) - (Math.abs(-(window.innerHeight * .5) + event.pageY) - 200);
+// Функция для запуска автоматической прокрутки
+function startAutoRotate() {
+  // Очищаем существующий интервал (если был)
+  if (autoSpinInterval) {
+    clearInterval(autoSpinInterval);
   }
+  // Запускаем новый интервал для автоматической прокрутки
+  autoSpinInterval = setInterval(() => {
+    changeCarouselIndex(); // Переход на следующий элемент
+  }, 3000); // Переход каждую секунду
 }
 
-// loops and sets the carousel 3d properties
-function looper() {
-  if (isMouseInsideCarousel) {
-    // накапливаем смещение по оси X, если курсор на карусели
-    addX += mouseX;
-  } else {
-    // продолжаем вращать карусель, если мышь не на ней
-    addX += 0.4; // настройте скорость вращения, если мышь не на карусели
+// Остановка автопрокрутки
+function stopAutoRotate() {
+  if (autoSpinInterval) {
+    clearInterval(autoSpinInterval);
   }
-
-  // применяем изменения
-  TweenMax.to(carousel, 1, {rotationY: addX, rotationX: mouseY, ease: Quint.easeOut});
-  TweenMax.set(carousel, {z: mouseZ});
-  fps.text('Framerate: ' + counter.tick() + '/60 FPS');
-}
-
-function getRandomInt($n) {
-  return Math.floor((Math.random() * $n) + 1);
 }
